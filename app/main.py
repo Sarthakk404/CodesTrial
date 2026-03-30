37b669bb11d2f84a922d256f2342c6bb829e0f63
Sarthak <sarthak.index@gmail.com>
2026-03-31 00:23:17 +0530

    CORD updated

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://resilient-adaptation-production-a17c.up.railway.app", "http://localhost:3000"],
+    allow_origins=["https://codes-trial.vercel.app/", "http://localhost:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
